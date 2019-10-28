8acfb60afdb32aac3abb5c1dba73390dbb160dcb
Troy Liu <troy.liu@mail.utoronto.ca>
2019-10-28 19:59:03 -0400

    Copied imports from Fish Tank, added a unrestrained button that does nothing to the main view.

--- phase1/UnlimitedAlienGames/app/src/main/java/com/example/unlimitedaliengames/MainActivity.java
+++ phase1/UnlimitedAlienGames/app/src/main/java/com/example/unlimitedaliengames/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.unlimitedaliengames;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
