93d303bebf2a7f77c03345059a67e31714e03c47
Tony <tonyyf.hong@mail.utoronto.ca>
2019-10-29 15:11:34 -0400

    Created separate packages containing activities (Views) for each level of the game

---
+++ phase1/UnlimitedAlienGames/app/src/main/java/com/example/unlimitedaliengames/AlienPainter/AlienPainter.java
@@ -0,0 +1,16 @@
+package com.example.unlimitedaliengames.AlienPainter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.unlimitedaliengames.R;
+
+public class AlienPainter extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_alien_painter);
+  }
+}
---
+++ phase1/UnlimitedAlienGames/app/src/main/java/com/example/unlimitedaliengames/AlienShooter/AlienShooter.java
@@ -0,0 +1,16 @@
+package com.example.unlimitedaliengames.AlienShooter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.unlimitedaliengames.R;
+
+public class AlienShooter extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_alien_shooter);
+  }
+}
