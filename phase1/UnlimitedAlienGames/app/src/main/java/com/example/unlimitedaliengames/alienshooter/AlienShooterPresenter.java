12fdf1a1502639d384ba586532d1a80d52d95b01
Tony <tonyyf.hong@mail.utoronto.ca>
2019-10-30 15:31:40 -0400

    Minor fixes

--- phase1/UnlimitedAlienGames/app/src/main/java/com/example/unlimitedaliengames/alienshooter/AlienShooterPresenter.java
+++ phase1/UnlimitedAlienGames/app/src/main/java/com/example/unlimitedaliengames/alienshooter/AlienShooterPresenter.java
@@ -1,4 +1,4 @@
 package com.example.unlimitedaliengames.alienshooter;
 
-class Presenter {
+class AlienShooterPresenter {
 }
--- phase1/UnlimitedAlienGames/app/src/main/java/com/example/unlimitedaliengames/alienshooter/AlienShooterView.java
+++ phase1/UnlimitedAlienGames/app/src/main/java/com/example/unlimitedaliengames/alienshooter/AlienShooterView.java
@@ -2,18 +2,27 @@ package com.example.unlimitedaliengames.alienshooter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import com.example.unlimitedaliengames.R;
 
-public class AlienShooter extends AppCompatActivity {
+import java.util.List;
+
+public class AlienShooterView extends AppCompatActivity {
+
+    private List<ImageButton> aliens;
+    private AlienShooterPresenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alien_shooter);
 
-        Intent intent = getIntent();  //Leaving this here in case we need to send something through the intent in the future
+
     }
+    void changeAlien(){
+
+    }
+
 }
