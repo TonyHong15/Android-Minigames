3446113d9e1cb5646e94577c4f74a9192c7a8610
woojiin <zzangwooojiin@gmail.com>
2019-10-30 18:00:26 -0400

    add a timer in the alien shooter game

--- phase1/UnlimitedAlienGames/app/src/main/java/com/example/unlimitedaliengames/alienshooter/AlienShooter.java
+++ phase1/UnlimitedAlienGames/app/src/main/java/com/example/unlimitedaliengames/alienshooter/AlienShooter.java
@@ -3,7 +3,11 @@ package com.example.unlimitedaliengames.alienshooter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.example.unlimitedaliengames.R;
 
@@ -14,14 +18,47 @@ public class AlienShooter extends AppCompatActivity {
     private List<ImageButton> aliens;
     private AlienShooterPresenter presenter;
 
+    //timer
+    private long time_left = 30000;
+    private TextView timer_text;
+    private Button timer_button;
+    private boolean use_timer = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alien_shooter);
 
+        timer_text = findViewById(R.id.alienTimer);
+        timer_button = findViewById(R.id.timer_button);
+
+        timer_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (use_timer)
+                    startTimer();
+            }
+        });
 
     }
-    void changeAlien(){
+
+    public void startTimer() {
+        use_timer = false;
+        timer_button.setVisibility(View.INVISIBLE);
+        new CountDownTimer(time_left, 1000) {
+
+            public void onTick(long seconds) {
+                String text = "Time: " + seconds / 1000;
+                timer_text.setText(text);
+            }
+
+            public void onFinish() {
+            }
+        }.start();
+
+    }
+
+    void changeAlien() {
 
     }
 
