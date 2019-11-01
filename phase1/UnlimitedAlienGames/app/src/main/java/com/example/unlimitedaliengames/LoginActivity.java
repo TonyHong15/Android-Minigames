3013a06222d1dedbdd5fc4d975b630c2d666e382
Troy Liu <troy.liu@mail.utoronto.ca>
2019-10-31 22:50:48 -0400

    Added fake login.

--- phase1/UnlimitedAlienGames/app/src/main/java/com/example/unlimitedaliengames/LoginActivity.java
+++ phase1/UnlimitedAlienGames/app/src/main/java/com/example/unlimitedaliengames/LoginActivity.java
@@ -1,6 +1,53 @@
 package com.example.unlimitedaliengames;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.unlimitedaliengames.alienguesser.AlienGuesser;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText userName;
+    private EditText password;
+    private Button login;
+    private Button register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        userName = findViewById(R.id.userName);
+        password = findViewById(R.id.password);
+        login = findViewById(R.id.loginButton);
+        register = findViewById(R.id.registerButton);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onSuccess();
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                attemptRegister();
+            }
+        });
+    }
+
+    void onSuccess(){
+        startActivity(new Intent(this, MainActivity.class));
+    }
+
+    void attemptRegister(){
+        String user = userName.getText().toString();
+        String pass = password.getText().toString();
+    }
 }
