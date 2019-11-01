c22431a7ab8db50495b16bd73fb8fd12491c0e6d
Tony <tonyyf.hong@mail.utoronto.ca>
2019-11-01 16:37:14 -0400

    Set on click listeners for all customization buttons

--- phase1/UnlimitedAlienGames/app/src/main/java/com/example/unlimitedaliengames/alienshooter/CustomizeActivity.java
+++ phase1/UnlimitedAlienGames/app/src/main/java/com/example/unlimitedaliengames/alienshooter/CustomizeActivity.java
@@ -16,5 +16,100 @@ public class CustomizeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customize);
+
+        fifteen = findViewById(R.id.FIFTEEN);
+        setListenerFifteen();
+        thirty = findViewById(R.id.THIRTY);
+        setListenerThirty();
+        red = findViewById(R.id.RED);
+        setListenerRed();
+        black = findViewById(R.id.BLACK);
+        setListenerBlack();
+        blue = findViewById(R.id.BLUE);
+        setListenerBlue();
+        yellow = findViewById(R.id.YELLOW);
+        setListenerYellow();
+        proceed = findViewById(R.id.proceed);
+
+        choseEvil = false;
+        choseFriendly = false;
+        choseTime = false;
+
+        time = null;
+        friendly = null;
+        evil = null;
+    }
+
+    private void setListenerFifteen(){
+        fifteen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!choseTime) {
+                    time = fifteen;
+                    thirty.setVisibility(View.INVISIBLE);
+                    choseTime = true;
+                }
+            }
+        });
+    }
+    private void setListenerThirty(){
+        thirty.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!choseTime) {
+                    time = thirty;
+                    fifteen.setVisibility(View.INVISIBLE);
+                    choseTime = true;
+                }
+            }
+        });
+    }
+    private void setListenerBlue(){
+        blue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!choseFriendly) {
+                    friendly = blue;
+                    yellow.setVisibility(View.INVISIBLE);
+                    choseFriendly = true;
+                }
+            }
+        });
+    }
+    private void setListenerYellow(){
+        yellow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!choseFriendly) {
+                    friendly = yellow;
+                    blue.setVisibility(View.INVISIBLE);
+                    choseFriendly = true;
+                }
+            }
+        });
+    }
+    private void setListenerRed(){
+        red.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!choseEvil) {
+                    evil = red;
+                    black.setVisibility(View.INVISIBLE);
+                    choseEvil = true;
+                }
+            }
+        });
+    }
+    private void setListenerBlack(){
+        black.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!choseEvil) {
+                    evil = black;
+                    red.setVisibility(View.INVISIBLE);
+                    choseEvil = true;
+                }
+            }
+        });
     }
 }
